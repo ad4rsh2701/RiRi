8b6fabd3f6eaa2847bd86fe06d1847db5af92499
adarsh2701 <adarsharyan2701@gmail.com>
2026-05-25 02:30:39 +0530

    feat(delete.cpp): add implementation for `DELETE` command in `delete.cpp`
    - Implemented overloaded functions of the `DELETE` command (`Status`, `StatusErrorBatchWith`, `StatusBatchWith`).
    
    ;-;

---
+++ src/commands/delete.cpp
@@ -0,0 +1,42 @@
+#include <Commands.hpp>
+#include <DataManager.h>
+
+namespace RiRi::Commands {
+
+    Response::Status DELETE (std::string_view key) {
+        return Response::Status(Internal::deleteKey(key)
+            ? Response::StatusCode::OK
+            : Response::StatusCode::ERR_KEY_NOT_FOUND);
+    }
+
+    Response::Status DELETE (std::span<RapidNode> nodes) {
+        Response::Status response;
+        for (auto& [key, _]: nodes) {
+            if (const bool success = Internal::deleteKey(key); !success) {
+                response.setCode(Response::StatusCode::ERR_SOME_OPERATIONS_FAILED);
+            }
+        }
+        return response;
+    }
+
+    Response::StatusErrorBatchWith<std::string_view> DELETE (std::span<RapidNode> nodes, enableErrorBatched) {
+        Response::StatusErrorBatchWith<std::string_view> response;
+        for (auto& [key, _]: nodes) {
+            if (const bool success = Internal::deleteKey(key); !success) {
+                response.addErrorEntry(key, Response::StatusCode::ERR_KEY_NOT_FOUND);
+            }
+        }
+        return response;
+    }
+
+    Response::StatusBatchWith<std::string_view, std::monostate> DELETE (std::span<RapidNode> nodes, enableBatched) {
+        Response::StatusBatchWith<std::string_view, std::monostate> response;
+        for (auto& [key, _]: nodes) {
+            if (const bool success = Internal::deleteKey(key); !success) {
+                response.addStatusEntry(key, Response::StatusCode::ERR_KEY_NOT_FOUND);
+            }
+        }
+        return response;
+    }
+
+} // namespace RiRi::Commands
