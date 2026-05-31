e927bd3b48dac8f470d2cdb19a237a7b87c7c3ed
adarsh2701 <adarsharyan2701@gmail.com>
2026-05-31 12:37:12 +0530

    chore(include): change case of aggregator headers to be snake case and move `RiRi.hpp` under include dir
    Basically, the headers that just import other headers to provide ease of import will not be of snake case (matching the case style of the dirs).
    
    `#include <riri/Commands.hpp>` makes sense only if `#include <riri.hpp>` exists. Same logic goes for `utils.hpp`.

---
+++ include/riri.hpp
@@ -0,0 +1,8 @@
+#pragma once    // RIRI.HPP
+
+// CORE
+#include "riri/Commands.hpp"
+#include "riri/RapidResponse.hpp"
+
+// UTILS
+#include "riri/utils/Accessors.hpp"
--- include/riri/RiRi.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once    // RIRI.HPP
-
-// CORE
-#include "Commands.hpp"
-#include "RapidResponse.hpp"
-
-// UTILS
-#include "utils/Accessors.hpp"
