499874ecdebf69900b970ef0c09d6baae589cc61
adarsh2701 <adarsharyan2701@gmail.com>
2026-06-28 16:39:39 +0530

    refactor(riri.hpp): add C++23 requirement and add version.hpp include

--- include/riri.hpp
+++ include/riri.hpp
@@ -1,8 +1,26 @@
+#ifndef RIRI_HPP_INCLUDED
+#define RIRI_HPP_INCLUDED
+
 #pragma once    // RIRI.HPP
 
-// CORE
+// #################################################### C++23 ####################################################### //
+
+#if __cplusplus < 202302L
+#   error "RiRi requires C++23 or higher"
+#endif
+
+// ################################################### IMPORTS ###################################################### //
+
+    // CORE
 #include "riri/Commands.hpp"
 #include "riri/RapidResponse.hpp"
 
-// UTILS
+    // UTILS
 #include "riri/utils/Accessors.hpp"
+
+    // VERSION
+#include "version.hpp"
+
+// ################################################################################################################## //
+
+#endif // RIRI_HPP_INCLUDED
